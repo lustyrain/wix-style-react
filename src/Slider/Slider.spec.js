2ce197ce8ba48f196e627407e4b1f0f457995744
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-02-09 14:30:21 +0200

    displaying tooltip label when hovering handle

--- src/Slider/Slider.spec.js
+++ src/Slider/Slider.spec.js
@@ -100,7 +100,15 @@ describe('Slider', () => {
         expect(await driver.numOfSliderMarksLabel()).toBe(6);
       });
 
-      it('should display the mark label when hovering the slider handle', async () => {});
+      it('should display the mark label when hovering the slider handle', async () => {
+        const onChange = jest.fn(value => this.setState({ value }));
+        const props = { marks, onChange, value: 2 };
+        const { driver } = render(<Slider {...props} />);
+
+        await driver.hoverHandle({ handleIndex: 0 });
+
+        expect(await driver.getToolTipValue()).toBe(marks[2]);
+      });
 
       it('should not display tooltip if the handler has value with no mark label', async () => {});
     });
